8cce06986acf8715b29e3cd544dfad9db896140c
Suratchanan Kraidech <benchbb01@hotmail.com>
2014-10-17 23:15:34 +0700

    Prepare graph ingredient

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -2,7 +2,41 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include<vector>
+#include<map>
 using namespace std;
+
+class Vertex{
+public:
+	char vname;
+	Vertex(char ch){
+		vname = ch;
+	}
+};
+
+map<Vertex, int> dist;
+map<Vertex, Vertex> prev;
+
+
+class Edge{
+public:
+	Vertex source;
+	Vertex dest;
+	int dist;
+};
+
+class Graph{
+public :
+	vector<Vertex> vertex;
+	Vertex find(char vertexname){
+		for (Vertex x : vertex){
+			if (x.vname == vertexname)
+				return x;
+		}
+		return 0;
+	}
+};
+
 
 int stringToInteger(string str);
 void main(){
@@ -10,6 +44,7 @@ void main(){
 	int graph[12][12];
 	ifstream infile;
 	string str,input;
+	Graph g;
 	infile.open("graphTOC.csv");
 	if (infile.fail())
 		cerr << "Fail to open file.";
@@ -22,9 +57,16 @@ void main(){
 				input = input + str[i];
 				graph[y][z++] = stringToInteger(input);
 			}
+			else if (isalpha(str[i])){
+				Vertex r(str[i]);
+				g.vertex.push_back(r);
+			}
 		}
 		y++;
 	}
+	for (vector<Vertex>::iterator it = g.vertex.begin(); it != g.vertex.end(); it++)
+		cout << (*it).vname << ' ';
+	cout << endl;
 	for (int i = 0; i < 12; i++){
 		for (int j = 0; j < 12; j++)
 			cout << graph[i][j] << " ";
